e4469a36401ba5fde1f9fbaffbe0aae23bab2cf9
sureshchithram <104250256+sureshchithram@users.noreply.github.com>
2022-04-26 21:35:33 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/usr/bin/echo "-----------FIREWALL CONFIGURATION-------" > /tmp/buildlog
+/usr/bin/systemctl disable firewalld
+/usr/bin/systemctl status firewalld >> /tmp/buildlog
+/usr/sbin/iptables -F
+/usr/sbin/sestatus >> /tmp/buildlog
+
+/usr/bin/echo "-----------HOSTNAME CONFIGURATION -------" >> /tmp/buildlog
+/usr/bin/hostname suri.ashok.ins.com
+/usr/bin/cat /etc/hostname >> /tmp/buildlog
+/usr/bin/echo "192.168.1.118    suri.ashok.ins.com      suri" >> /etc/hosts
+/usr/bin/cat /etc/hosts >> /tmp/buildlog
+
+/usr/bin/echo "------NETWORK SETUP-----" >> /tmp/buildlog
+/usr/bin/cat /etc/sysconfig/network-scripts/ifcfg-roll >/etc/sysconfig/network-scripts/ifcfg-ens33
+/usr/bin/cat /etc/sysconfig/network-scripts/ifcfg-ens33 >> /tmp/buildlog
+
+/usr/bin/echo "---------NETWOR INTERFACE--------" >>/tmp/buildlog
+/usr/sbin/ifconfig -a >>/tmp/buildlog
+
+/usr/bin/echo "----------DISK PARTITION---------" >> /tmp/buildlog
+/usr/sbin/parted -l >> /tmp/buildlog
+
+/usr/bin/echo "------MOUNT FILE SYSTEM-------" >> /tmp/buildlog
+/usr/bin/df -h >> /tmp/buildlog
